bf75d17349f64e1a2cf7c9b221cf2fc26291a7c0
kulrisa22 <40323@prot.ac.th>
2025-03-11 15:30:27 +0700

    Delete cell03/ex00/to25.py

--- cell03/ex00/to25.py
+++
@@ -1,7 +0,0 @@
-number = int(input("Enter a number less than 25/n"))
-
-if number > 25:
-    print("Error")
-else:
-    for i in range(number,26):
-        print("Inside the loop, my variable is", i)
