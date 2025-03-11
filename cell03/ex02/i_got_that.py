9e73d05854db262911437955b703c87f5122fe70
kulrisa22 <40323@prot.ac.th>
2025-03-11 23:35:23 +0700

    Add files via upload

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,4 @@
+while True:
+    user_input = input( "What you gotta say ?: ")                                                                                                                                                  
+    if user_input.strip() ==  "STOP": break
+    print("I got that! Anything else?: ", user_input)
