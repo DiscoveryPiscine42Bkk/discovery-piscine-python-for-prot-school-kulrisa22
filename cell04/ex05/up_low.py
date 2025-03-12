0b20b489d6580da2f940624a1b8d2335baf445c9
kulrisa22 <40323@prot.ac.th>
2025-03-12 10:43:17 +0700

    Add files via upload

---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,12 @@
+string = input("Enter a string: ")
+result = ""
+
+for char in string:
+    if char.isupper():
+       result += char.lower()
+    elif char.islower():
+        result += char.upper()
+    else:
+        result += char
+        
+print(result)
