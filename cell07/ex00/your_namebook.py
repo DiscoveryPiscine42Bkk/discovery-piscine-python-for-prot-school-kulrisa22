40457b796f13b88007f7a43559a28d9e454bb2eb
kulrisa22 <40323@prot.ac.th>
2025-03-13 11:55:24 +0700

    Add files via upload

---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,15 @@
+def array_of_names(persons):
+    full_names = []
+    for first_name, last_name in persons.items():
+        full_name = first_name.capitalize() + " " + last_name.capitalize()
+        full_names.append(full_name)
+    return full_names
+
+persons = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "zavier": "niel",
+    "fifi": "brindacier"
+    }
+
+print(array_of_names(persons))
